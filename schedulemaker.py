5359271ca63d4502c8ee5323f8f40ae6449e9a07
iamethanclaire <ethanedwardclaire@gmail.com>
2026-01-31 14:57:21 -0800

    Finished

--- schedulemaker.py
+++ schedulemaker.py
@@ -3,32 +3,37 @@ from bs4 import BeautifulSoup
 import csv
 
 def main():
-    classdict = []  #reading to dict because finding values will be fast
+    classdict = []
+    finalsdict = []
     with open('classschedule.csv', newline='') as classschedule:
         spamreader = csv.DictReader(classschedule, delimiter=',')
         for row in spamreader:
             classdict.append(row)
-
-    print(classdict)
 
     r = requests.get('https://www.unr.edu/admissions/records/academic-calendar/finals-schedule')
     soup = BeautifulSoup(r.text, 'html.parser')
 
     elements = soup.find_all(class_='footable')
 
-    # for element in elements:
-    single_day = elements[0].get_text(strip=False)
-    generate_days_schedule(single_day, classdict)
+    #UNCOMMENT THIS WHEN READY TO MAKE FULL SCHEDULE
+    for element in elements:
+        single_day = element.get_text(strip=False)
+        generate_days_schedule(single_day, classdict, finalsdict)
+    writefile(finalsdict)
 
+def generate_days_schedule(single_day, classdict, finalsdict):
+        lines = single_day.splitlines()
+        for i, line in enumerate(lines):
+            for classinfo in classdict:
+                if classinfo['StartingTime'] in line:
+                    if i+2 < len(lines) and classinfo['DayOfWeek'] in lines[i+1]:
+                        finalsdict.append([classinfo['Class'],lines[0].strip(),lines[i+2].strip()])
 
-def generate_days_schedule(single_day, classdict):
-    lines = single_day.splitlines()
-    for i, line in enumerate(lines):
-        if line == '8:30 a.m.':
-            if i+2 < len(lines) and lines[i+1] == 'Tuesday/Thursday (TR)':
-                print(lines[i+2])
-
-    return 0
+def writefile(finalsdict):
+    with open('finalsschedule.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['Class', 'Day', 'Time'])
+        writer.writerows(finalsdict)
 
 if __name__ == '__main__':
     main()
